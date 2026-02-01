baadefc2fe864e04b7e517cb1ab722504272a845
brybryyyy12 <briansalise4@gmail.com>
2026-02-01 22:15:14 +0800

    took it back

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,21 +1,13 @@
 import mongoose from 'mongoose';
 
-let isConnected = false; // Track if already connected
-
-const conn = async () => {
-  if (isConnected) return; // Reuse existing connection
-
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      // optional, but recommended
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = true;
+const conn = async()=>{
+  try{
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DATABASE CONNECTED SUCCESSFULLY!");
-  } catch (err) {
-    console.log("DATABASE CONNECTION FAILED!", err);
+  }catch(err){
+    console.log("DATABASE CONNECTION FAILED!",err);
   }
-};
+}
+
 
 export default conn;
