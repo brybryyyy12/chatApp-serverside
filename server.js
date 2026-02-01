b5f4e7662b2dbef38f098a1950cb3770179d356c
brybryyyy12 <briansalise4@gmail.com>
2026-02-01 21:04:19 +0800

    fixed cors 2.0

--- server.js
+++ server.js
@@ -21,13 +21,8 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"],     // allow headers your frontend sends
 }));
 
-// Handle preflight requests manually (important on some platforms)
-app.options("*", cors({
-  origin: "https://chat-app-clientside.vercel.app",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
+
+
 
 app.use(express.json());
 
