38b461f352a6da6e918a2e4835eac29877957b22
brybryyyy12 <briansalise4@gmail.com>
2026-02-01 20:39:54 +0800

    fixed frontend url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 
 // setup socket.io
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://chat-app-clientside.vercel.app" },
 });
 
 // attach io to app so controllers can access it
